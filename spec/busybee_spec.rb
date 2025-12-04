e8ad5a1339d99d6b74a32aa7610466c0f1331da4
Andy Rusterholz <andyrusterholz@gmail.com>
2025-12-04 14:46:39 -0700

    add CI workflow

--- spec/busybee_spec.rb
+++ spec/busybee_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Busybee do
   it "has a version number" do
     expect(Busybee::VERSION).not_to be_nil
   end
-
-  it "does something useful" do
-    expect(false).to be(true) # rubocop:disable RSpec/ExpectActual
-  end
 end
